6f8167983c364ee61864e623663491ee11f4333f
Michael Jochimsen <mlj@troglodytics.org>
2020-02-16 12:15:38 -0800

    Split `webpack.config.js` into development and production configs.
    Also add `"release"` and `"clean"` scripts to `package.json`, as well as
    tweaking the existing scripts to use the new configurations.

--- webpack.common.js
+++ webpack.common.js
@@ -2,15 +2,10 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
-  },
-  devtool: 'inline-source-map',
-  devServer: {
-    contentBase: './dist'
   },
   module: {},
   plugins: [
